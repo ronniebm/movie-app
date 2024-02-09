fe3e477e0c963d320891d900a11fc7ab74e2b32a
Ronnie Barrios Morales <ronniebarriosmorales@192.168.1.14>
2024-02-09 18:14:31 -0500

    feature/mv01: test05

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     '/movie-details': {
-  //       target: 'https://movie-app-ronnie.netlify.app',
-  //       rewrite: (path) => path.replace(/^\/movie-details/, ''),
-  //     },
-  //   },
-  // },
+  server: {
+    proxy: {
+      '/movie-details': {
+        target: 'https://deploy-preview-5--movie-app-ronnie.netlify.app',
+        rewrite: (path) => path.replace(/^\/movie-details/, ''),
+      },
+    },
+  },
 })
